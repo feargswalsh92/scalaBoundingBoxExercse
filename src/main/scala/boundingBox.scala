5ce5feedba41ab4981d872f8ac4097f7cc403b2a
Feargal Walsh <feargswalsh@gmail.com>
2017-02-28 22:49:18 -0600

    Half of ellipse test passing

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -5,7 +5,7 @@ package edu.luc.cs.laufer.cs372.shapes
 object boundingBox {
   def apply(s: Shape): Location = s match {
     case Rectangle(_, _)  => Location(0, 0, s)
-    case Ellipse(min,maj) =>  Location(-min,maj,Rectangle(-min,-maj))
+    case Ellipse(min,maj) =>  Location(0-min,0-maj,Rectangle(0-min,0-maj))
     //receiving error requiring me to pass a locaiton
     case _ => Location(0, 0, Rectangle(0, 0))
 
